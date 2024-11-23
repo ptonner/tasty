ba346f22a46c8be79b23514f254194ad9a5b6529
Peter Tonner <peter.tonner@proton.me>
2024-11-22 22:29:13 -0500

    mini gud

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,214 @@
 mod toy;
 mod watch;
 
-fn main() {
-    let path = std::env::args()
-        .nth(1)
-        .expect("Argument 1 needs to be a path");
+use miniquad::*;
 
-    toy::create_toy(&path);
-
-    futures::executor::block_on(async {
-        if let Err(e) = watch::async_watch(path).await {
-            println!("error: {:?}", e)
-        }
-    });
+#[repr(C)]
+struct Vec2 {
+    x: f32,
+    y: f32,
 }
+#[repr(C)]
+struct Vertex {
+    pos: Vec2,
+    uv: Vec2,
+}
+
+struct Stage {
+    ctx: Box<dyn RenderingBackend>,
+
+    pipeline: Pipeline,
+    bindings: Bindings,
+}
+
+impl Stage {
+    pub fn new() -> Stage {
+        let mut ctx: Box<dyn RenderingBackend> = window::new_rendering_backend();
+
+        #[rustfmt::skip]
+        let vertices: [Vertex; 4] = [
+            Vertex { pos : Vec2 { x: -1.0, y: -1.0 }, uv: Vec2 { x: 0., y: 0. } },
+            Vertex { pos : Vec2 { x:  1.0, y: -1.0 }, uv: Vec2 { x: 1., y: 0. } },
+            Vertex { pos : Vec2 { x:  1.0, y:  1.0 }, uv: Vec2 { x: 1., y: 1. } },
+            Vertex { pos : Vec2 { x: -1.0, y:  1.0 }, uv: Vec2 { x: 0., y: 1. } },
+        ];
+        let vertex_buffer = ctx.new_buffer(
+            BufferType::VertexBuffer,
+            BufferUsage::Immutable,
+            BufferSource::slice(&vertices),
+        );
+
+        let indices: [u16; 6] = [0, 1, 2, 0, 2, 3];
+        let index_buffer = ctx.new_buffer(
+            BufferType::IndexBuffer,
+            BufferUsage::Immutable,
+            BufferSource::slice(&indices),
+        );
+
+        let bindings = Bindings {
+            vertex_buffers: vec![vertex_buffer],
+            index_buffer: index_buffer,
+            images: vec![],
+        };
+
+        let shader = ctx
+            .new_shader(
+                match ctx.info().backend {
+                    Backend::OpenGl => ShaderSource::Glsl {
+                        vertex: shader::VERTEX,
+                        fragment: shader::FRAGMENT,
+                    },
+                    Backend::Metal => ShaderSource::Msl {
+                        program: shader::METAL,
+                    },
+                },
+                shader::meta(),
+            )
+            .unwrap();
+
+        let pipeline = ctx.new_pipeline(
+            &[BufferLayout::default()],
+            &[
+                VertexAttribute::new("in_pos", VertexFormat::Float2),
+                VertexAttribute::new("in_uv", VertexFormat::Float2),
+            ],
+            shader,
+            PipelineParams::default(),
+        );
+
+        Stage {
+            pipeline,
+            bindings,
+            ctx,
+        }
+    }
+}
+
+impl EventHandler for Stage {
+    fn update(&mut self) {}
+
+    fn draw(&mut self) {
+        let t = date::now();
+
+        self.ctx.begin_default_pass(Default::default());
+
+        self.ctx.apply_pipeline(&self.pipeline);
+        self.ctx.apply_bindings(&self.bindings);
+        self.ctx
+            .apply_uniforms(UniformsSource::table(&shader::Uniforms {
+                offset: (0.0, 0.0),
+            }));
+        self.ctx.draw(0, 6, 1);
+        self.ctx.end_render_pass();
+
+        self.ctx.commit_frame();
+    }
+}
+
+fn main() {
+    let mut conf = conf::Conf::default();
+    let metal = std::env::args().nth(1).as_deref() == Some("metal");
+    conf.platform.apple_gfx_api = if metal {
+        conf::AppleGfxApi::Metal
+    } else {
+        conf::AppleGfxApi::OpenGl
+    };
+
+    miniquad::start(conf, move || Box::new(Stage::new()));
+}
+
+mod shader {
+    use miniquad::*;
+
+    pub const VERTEX: &str = r#"#version 100
+    attribute vec2 in_pos;
+    attribute vec2 in_uv;
+
+    //uniform vec2 offset;
+
+    varying lowp vec2 texcoord;
+
+    void main() {
+        gl_Position = vec4(in_pos, 0, 1);
+        texcoord = in_uv;
+    }"#;
+
+    pub const FRAGMENT: &str = r#"#version 100
+    varying lowp vec2 texcoord;
+
+    // uniform sampler2D tex;
+
+    void main() {
+        // gl_FragColor = texture2D(tex, texcoord);
+        // gl_FragColor = vec4(texcoord.x, texcoord.y, 1, 1);
+        gl_FragColor = vec4(texcoord, 0, 0);
+    }"#;
+
+    pub const METAL: &str = r#"
+    #include <metal_stdlib>
+
+    using namespace metal;
+
+    struct Uniforms
+    {
+        float2 offset;
+    };
+
+    struct Vertex
+    {
+        float2 in_pos   [[attribute(0)]];
+        float2 in_uv    [[attribute(1)]];
+    };
+
+    struct RasterizerData
+    {
+        float4 position [[position]];
+        float2 uv       [[user(locn0)]];
+    };
+
+    vertex RasterizerData vertexShader(
+      Vertex v [[stage_in]], 
+      constant Uniforms& uniforms [[buffer(0)]])
+    {
+        RasterizerData out;
+
+        out.position = float4(v.in_pos.xy + uniforms.offset, 0.0, 1.0);
+        out.uv = v.in_uv;
+
+        return out;
+    }
+
+    fragment float4 fragmentShader(RasterizerData in [[stage_in]], texture2d<float> tex [[texture(0)]], sampler texSmplr [[sampler(0)]])
+    {
+        return tex.sample(texSmplr, in.uv);
+    }"#;
+
+    pub fn meta() -> ShaderMeta {
+        ShaderMeta {
+            // images: vec!["tex".to_string()],
+            images: vec![],
+            uniforms: UniformBlockLayout {
+                uniforms: vec![UniformDesc::new("offset", UniformType::Float2)],
+            },
+        }
+    }
+
+    #[repr(C)]
+    pub struct Uniforms {
+        pub offset: (f32, f32),
+    }
+}
+
+// fn main() {
+//     let path = std::env::args()
+//         .nth(1)
+//         .expect("Argument 1 needs to be a path");
+
+//     toy::create_toy(&path);
+
+//     futures::executor::block_on(async {
+//         if let Err(e) = watch::async_watch(path).await {
+//             println!("error: {:?}", e)
+//         }
+//     });
+// }
