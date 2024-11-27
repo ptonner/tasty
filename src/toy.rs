85b24fc4bab8a86ed00dcf3b14cc91fecfa8e544
Peter Tonner <peter.tonner@proton.me>
2024-11-26 20:46:13 -0500

    basic recompilation on file change

--- src/toy.rs
+++ src/toy.rs
@@ -12,9 +12,9 @@ use notify::{Error, Event, EventKind};
 pub fn create_toy(path: &String) {
     fs::create_dir_all(path).expect("directory accessible");
     let path = PathBuf::from(path);
-    fs::write(path.join("toy.glsl"), DEFAULT_TOY_SHADER).expect("toy writeable");
-    fs::write(path.join("vertex.glsl"), DEFAULT_VERTEX_SHADER).expect("toy writeable");
-    fs::write(path.join("fragment.glsl"), DEFAULT_FRAGMENT_SHADER).expect("toy writeable");
+    fs::write(path.join("toy.glsl"), MAIN_IMAGE).expect("toy writeable");
+    // fs::write(path.join("vertex.glsl"), DEFAULT_VERTEX_SHADER).expect("toy writeable");
+    // fs::write(path.join("fragment.glsl"), DEFAULT_FRAGMENT_SHADER).expect("toy writeable");
 }
 
 pub const VERTEX: &str = r#"#version 330
@@ -30,9 +30,16 @@ pub const VERTEX: &str = r#"#version 330
         texcoord = in_uv;
     }"#;
 
-pub const MAIN_IMAGE: &str = r#"void mainImage(out vec4 fragColor, in vec2 fragCoord)
+pub const MAIN_IMAGE: &str = r#"void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    fragColor = vec4(1, 1, 1, 1);
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    // Output to screen
+    fragColor = vec4(col,1.0);
 }"#;
 
 pub fn build_fragment_shader(main_image: &str) -> String {
@@ -75,10 +82,246 @@ pub fn meta() -> ShaderMeta {
     }
 }
 
-const DEFAULT_TOY_SHADER: &str = "void mainImage(out vec4 fragColor, in vec2 fragCoord)
-{    
-}";
+#[repr(C)]
+struct Vec2 {
+    x: f32,
+    y: f32,
+}
 
+#[repr(C)]
+struct Vertex {
+    pos: Vec2,
+    uv: Vec2,
+}
+
+#[repr(C)]
+pub struct Uniforms {
+    pub iResolution: (f32, f32, f32),
+    pub iMouse: (f32, f32, f32, f32),
+    pub iTime: f32,
+    pub iTimeDelta: f32,
+    pub iFrame: i32,
+    pub iFrameRate: f32,
+    // TODO
+    // iChannelTime
+    // iChannelResolution
+    // iChannel
+    // iDate
+    // iSampleRate
+}
+
+enum MouseState {
+    Down { x: f32, y: f32 },
+    Up,
+}
+
+pub struct Stage {
+    ctx: Box<dyn RenderingBackend>,
+    pipeline: Pipeline,
+    bindings: Bindings,
+    uniforms: Uniforms,
+    start: SystemTime,
+    last_frame: SystemTime,
+    mouse_state: MouseState,
+    receiver: Receiver<Result<Event, Error>>,
+}
+
+impl Stage {
+    pub fn new(rx: Receiver<Result<Event, Error>>) -> Stage {
+        let mut ctx: Box<dyn RenderingBackend> = window::new_rendering_backend();
+        window::show_mouse(false);
+
+        #[rustfmt::skip]
+        let vertices: [Vertex; 4] = [
+            Vertex { pos : Vec2 { x: -1.0, y: -1.0 }, uv: Vec2 { x: 0., y: 0. } },
+            Vertex { pos : Vec2 { x:  1.0, y: -1.0 }, uv: Vec2 { x: 1., y: 0. } },
+            Vertex { pos : Vec2 { x:  1.0, y:  1.0 }, uv: Vec2 { x: 1., y: 1. } },
+            Vertex { pos : Vec2 { x: -1.0, y:  1.0 }, uv: Vec2 { x: 0., y: 1. } },
+        ];
+        let vertex_buffer = ctx.new_buffer(
+            BufferType::VertexBuffer,
+            BufferUsage::Immutable,
+            BufferSource::slice(&vertices),
+        );
+
+        let indices: [u16; 6] = [0, 1, 2, 0, 2, 3];
+        let index_buffer = ctx.new_buffer(
+            BufferType::IndexBuffer,
+            BufferUsage::Immutable,
+            BufferSource::slice(&indices),
+        );
+
+        let bindings = Bindings {
+            vertex_buffers: vec![vertex_buffer],
+            index_buffer,
+            images: vec![],
+        };
+
+        let fragment = build_fragment_shader(MAIN_IMAGE);
+        // let fragment = build_fragment_shader(TEST_TOY);
+        let shader = ctx
+            .new_shader(
+                match ctx.info().backend {
+                    Backend::OpenGl => ShaderSource::Glsl {
+                        vertex: VERTEX,
+                        fragment: fragment.as_str(),
+                    },
+                    Backend::Metal => panic!("Metal not supported"),
+                },
+                meta(),
+            )
+            .unwrap();
+
+        let pipeline = ctx.new_pipeline(
+            &[BufferLayout::default()],
+            &[
+                VertexAttribute::new("in_pos", VertexFormat::Float2),
+                VertexAttribute::new("in_uv", VertexFormat::Float2),
+            ],
+            shader,
+            PipelineParams::default(),
+        );
+
+        let (w, h) = window::screen_size();
+        Stage {
+            pipeline,
+            bindings,
+            ctx,
+            uniforms: Uniforms {
+                iResolution: (w, h, 1.0),
+                iMouse: (0.0, 0.0, 0.0, 0.0),
+                iTime: 0.0,
+                iTimeDelta: 0.0,
+                iFrame: 0,
+                iFrameRate: 0.0,
+            },
+            start: SystemTime::now(),
+            last_frame: SystemTime::now(),
+            mouse_state: MouseState::Up,
+            receiver: rx,
+        }
+    }
+
+    fn handle_event(&mut self, event: Event) {
+        match event {
+            Event {
+                kind: EventKind::Modify(ModifyKind::Data(DataChange::Any)),
+                paths,
+                attrs: _,
+            } => {
+                let p = &paths[0];
+                match p.file_name().unwrap().to_owned().to_str().unwrap() {
+                    "toy.glsl" => match fs::read_to_string(p) {
+                        Ok(toy) => self.recompile(&toy),
+                        Err(err) => println!("Error reading {:?}: {:}", p, err),
+                    },
+                    _ => (),
+                }
+            }
+            _ => (),
+        }
+    }
+
+    fn recompile(&mut self, toy: &String) {
+        let fragment = build_fragment_shader(toy);
+        let shader = self
+            .ctx
+            .new_shader(
+                match self.ctx.info().backend {
+                    Backend::OpenGl => ShaderSource::Glsl {
+                        vertex: VERTEX,
+                        fragment: fragment.as_str(),
+                    },
+                    Backend::Metal => panic!("Metal not supported"),
+                },
+                meta(),
+            )
+            .unwrap();
+
+        let pipeline = self.ctx.new_pipeline(
+            &[BufferLayout::default()],
+            &[
+                VertexAttribute::new("in_pos", VertexFormat::Float2),
+                VertexAttribute::new("in_uv", VertexFormat::Float2),
+            ],
+            shader,
+            PipelineParams::default(),
+        );
+        self.pipeline = pipeline;
+    }
+}
+
+impl EventHandler for Stage {
+    fn update(&mut self) {
+        let now = (SystemTime::now()
+            .duration_since(self.start)
+            .expect("Linear time")
+            .as_millis() as f32)
+            / 1000.0;
+        let dt = (SystemTime::now()
+            .duration_since(self.last_frame)
+            .expect("Linear time")
+            .as_millis() as f32)
+            / 1000.0;
+        self.last_frame = SystemTime::now();
+        self.uniforms.iTime = now;
+        self.uniforms.iTimeDelta = dt;
+        self.uniforms.iFrame += 1;
+        self.uniforms.iFrameRate = 1.0 / dt;
+
+        match self.receiver.try_next() {
+            Ok(Some(Ok(evt))) => self.handle_event(evt),
+            Ok(Some(Err(err))) => println!("error: {:?}", err),
+            Ok(None) => println!("closed"),
+            Err(_e) => (),
+        }
+    }
+
+    fn draw(&mut self) {
+        self.ctx.begin_default_pass(Default::default());
+
+        self.ctx.apply_pipeline(&self.pipeline);
+        self.ctx.apply_bindings(&self.bindings);
+        self.ctx
+            .apply_uniforms(UniformsSource::table(&self.uniforms));
+        self.ctx.draw(0, 6, 1);
+        self.ctx.end_render_pass();
+
+        self.ctx.commit_frame();
+    }
+
+    fn resize_event(&mut self, _width: f32, _height: f32) {
+        self.uniforms.iResolution = (_width, _height, 1.0);
+    }
+
+    fn mouse_motion_event(&mut self, _x: f32, _y: f32) {
+        match self.mouse_state {
+            MouseState::Down { x, y } => {
+                let h = self.uniforms.iResolution.1;
+                self.uniforms.iMouse = (_x, h - _y, x, h - y);
+            }
+            _ => (),
+        }
+    }
+
+    fn mouse_button_down_event(&mut self, _button: MouseButton, _x: f32, _y: f32) {
+        match _button {
+            MouseButton::Left => {
+                self.mouse_state = MouseState::Down { x: _x, y: _y };
+            }
+            _ => (),
+        }
+    }
+
+    fn mouse_button_up_event(&mut self, _button: MouseButton, _x: f32, _y: f32) {
+        match _button {
+            MouseButton::Left => self.mouse_state = MouseState::Up,
+            _ => (),
+        }
+    }
+}
+
+#[allow(dead_code)]
 const TEST_TOY: &str = "
 // CC0: Starry planes
 //  A simple result that I think turned out pretty nice
@@ -270,237 +513,3 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
   fragColor = vec4(col, 1);
 }
 ";
-
-const DEFAULT_FRAGMENT_SHADER: &str = "#version 100
-precision lowp float;
-
-varying vec2 uv;
-
-uniform sampler2D Texture;
-
-void main() {
-    gl_FragColor = vec4(uv, 0, 0);
-}
-";
-
-const DEFAULT_VERTEX_SHADER: &str = "#version 100
-precision lowp float;
-
-attribute vec3 position;
-attribute vec2 texcoord;
-
-varying vec2 uv;
-
-void main() {
-    gl_Position = vec4(position, 1);
-    uv = texcoord;
-}
-";
-#[repr(C)]
-struct Vec2 {
-    x: f32,
-    y: f32,
-}
-
-#[repr(C)]
-struct Vertex {
-    pos: Vec2,
-    uv: Vec2,
-}
-
-#[repr(C)]
-pub struct Uniforms {
-    pub iResolution: (f32, f32, f32),
-    pub iMouse: (f32, f32, f32, f32),
-    pub iTime: f32,
-    pub iTimeDelta: f32,
-    pub iFrame: i32,
-    pub iFrameRate: f32,
-    // TODO
-    // iChannelTime
-    // iChannelResolution
-    // iChannel
-    // iDate
-    // iSampleRate
-}
-
-enum MouseState {
-    Down { x: f32, y: f32 },
-    Up,
-}
-
-pub struct Stage {
-    ctx: Box<dyn RenderingBackend>,
-    pipeline: Pipeline,
-    bindings: Bindings,
-    uniforms: Uniforms,
-    start: SystemTime,
-    last_frame: SystemTime,
-    mouse_state: MouseState,
-    receiver: Receiver<Result<Event, Error>>,
-}
-
-impl Stage {
-    pub fn new(rx: Receiver<Result<Event, Error>>) -> Stage {
-        let mut ctx: Box<dyn RenderingBackend> = window::new_rendering_backend();
-        window::show_mouse(false);
-
-        #[rustfmt::skip]
-        let vertices: [Vertex; 4] = [
-            Vertex { pos : Vec2 { x: -1.0, y: -1.0 }, uv: Vec2 { x: 0., y: 0. } },
-            Vertex { pos : Vec2 { x:  1.0, y: -1.0 }, uv: Vec2 { x: 1., y: 0. } },
-            Vertex { pos : Vec2 { x:  1.0, y:  1.0 }, uv: Vec2 { x: 1., y: 1. } },
-            Vertex { pos : Vec2 { x: -1.0, y:  1.0 }, uv: Vec2 { x: 0., y: 1. } },
-        ];
-        let vertex_buffer = ctx.new_buffer(
-            BufferType::VertexBuffer,
-            BufferUsage::Immutable,
-            BufferSource::slice(&vertices),
-        );
-
-        let indices: [u16; 6] = [0, 1, 2, 0, 2, 3];
-        let index_buffer = ctx.new_buffer(
-            BufferType::IndexBuffer,
-            BufferUsage::Immutable,
-            BufferSource::slice(&indices),
-        );
-
-        let bindings = Bindings {
-            vertex_buffers: vec![vertex_buffer],
-            index_buffer,
-            images: vec![],
-        };
-
-        // let fragment = build_fragment_shader(MAIN_IMAGE);
-        let fragment = build_fragment_shader(TEST_TOY);
-        let shader = ctx
-            .new_shader(
-                match ctx.info().backend {
-                    Backend::OpenGl => ShaderSource::Glsl {
-                        vertex: VERTEX,
-                        fragment: fragment.as_str(),
-                    },
-                    Backend::Metal => panic!("Metal not supported"),
-                },
-                meta(),
-            )
-            .unwrap();
-
-        let pipeline = ctx.new_pipeline(
-            &[BufferLayout::default()],
-            &[
-                VertexAttribute::new("in_pos", VertexFormat::Float2),
-                VertexAttribute::new("in_uv", VertexFormat::Float2),
-            ],
-            shader,
-            PipelineParams::default(),
-        );
-
-        let (w, h) = window::screen_size();
-        Stage {
-            pipeline,
-            bindings,
-            ctx,
-            uniforms: Uniforms {
-                iResolution: (w, h, 1.0),
-                iMouse: (0.0, 0.0, 0.0, 0.0),
-                iTime: 0.0,
-                iTimeDelta: 0.0,
-                iFrame: 0,
-                iFrameRate: 0.0,
-            },
-            start: SystemTime::now(),
-            last_frame: SystemTime::now(),
-            mouse_state: MouseState::Up,
-            receiver: rx,
-        }
-    }
-
-    fn handle_event(&mut self, event: Event) {
-        dbg!(&event);
-        match event {
-            Event {
-                kind: EventKind::Modify(ModifyKind::Data(DataChange::Any)),
-                paths,
-                attrs: _,
-            } => {
-                let p = &paths[0];
-                match p.file_name().unwrap().to_owned().to_str().unwrap() {
-                    "toy.glsl" => println!("toy"),
-                    _ => (),
-                }
-            }
-            _ => (),
-        }
-    }
-}
-
-impl EventHandler for Stage {
-    fn update(&mut self) {
-        let now = (SystemTime::now()
-            .duration_since(self.start)
-            .expect("Linear time")
-            .as_millis() as f32)
-            / 1000.0;
-        let dt = (SystemTime::now()
-            .duration_since(self.last_frame)
-            .expect("Linear time")
-            .as_millis() as f32)
-            / 1000.0;
-        self.last_frame = SystemTime::now();
-        self.uniforms.iTime = now;
-        self.uniforms.iTimeDelta = dt;
-        self.uniforms.iFrame += 1;
-        self.uniforms.iFrameRate = 1.0 / dt;
-
-        match self.receiver.try_next() {
-            Ok(Some(Ok(evt))) => self.handle_event(evt),
-            Ok(Some(Err(err))) => println!("error: {:?}", err),
-            Ok(None) => println!("closed"),
-            Err(_e) => (),
-        }
-    }
-
-    fn draw(&mut self) {
-        self.ctx.begin_default_pass(Default::default());
-
-        self.ctx.apply_pipeline(&self.pipeline);
-        self.ctx.apply_bindings(&self.bindings);
-        self.ctx
-            .apply_uniforms(UniformsSource::table(&self.uniforms));
-        self.ctx.draw(0, 6, 1);
-        self.ctx.end_render_pass();
-
-        self.ctx.commit_frame();
-    }
-
-    fn resize_event(&mut self, _width: f32, _height: f32) {
-        self.uniforms.iResolution = (_width, _height, 1.0);
-    }
-
-    fn mouse_motion_event(&mut self, _x: f32, _y: f32) {
-        match self.mouse_state {
-            MouseState::Down { x, y } => {
-                let h = self.uniforms.iResolution.1;
-                self.uniforms.iMouse = (_x, h - _y, x, h - y);
-            }
-            _ => (),
-        }
-    }
-
-    fn mouse_button_down_event(&mut self, _button: MouseButton, _x: f32, _y: f32) {
-        match _button {
-            MouseButton::Left => {
-                self.mouse_state = MouseState::Down { x: _x, y: _y };
-            }
-            _ => (),
-        }
-    }
-
-    fn mouse_button_up_event(&mut self, _button: MouseButton, _x: f32, _y: f32) {
-        match _button {
-            MouseButton::Left => self.mouse_state = MouseState::Up,
-            _ => (),
-        }
-    }
-}
